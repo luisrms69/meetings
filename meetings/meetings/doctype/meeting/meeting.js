ba969ab5409679ff256cbd5ff5fdd6a93e2a7008
Luis Montanaro Developer env <admin@appcity.mx>
2022-12-04 14:36:46 -0600

    LIMPIEZA quito codigo comentado en meeting.js

--- meetings/meetings/doctype/meeting/meeting.js
+++ meetings/meetings/doctype/meeting/meeting.js
@@ -1,28 +1,6 @@
 // Copyright (c) 2022, Buzola and contributors
 // For license information, please see license.txt
 
-/*
-frappe.ui.form.on('Meeting Attendees', {
-	attendee: function(frm, cdt, cdn){
-		var attendee = frappe.model.get_doc(cdt, cdn);
-		if (attendee.attendee) {
-			//if attendee, get full name
-			frappe.call({
-				method: "meetings.meetings.doctype.meeting.meeting.get_full_name",
-				args: {
-					attendee: attendee.attendee
-				},
-				callback: function(r) {
-					frappe.model.set_value(cdt, cdn, "full_name", r.message);
-				}
-			});
-		} else {
-			frappe.model.set_value(cdt, cdn, "full_name", null);
-		}
-	},
-});
-
-*/
 
 frappe.ui.form.on('Meeting', {
 	send_emails(frm) {
